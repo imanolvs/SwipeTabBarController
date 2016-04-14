1d11353b8a081b5cc8192328cef92ff6879e02ae
imanolvs <imanolvs@gmail.com>
2016-04-14 12:46:41 +0200

    Initial Commit

---
+++ SwipeTabBarController/SwipeTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  SwipeTabBarController.h
+//  SwipeTabBarController
+//
+//  Created by Imanol Viana Sánchez on 14/4/16.
+//  Copyright © 2016 Imanol Viana Sánchez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwipeTabBarController : NSObject
+
+@end
---
+++ SwipeTabBarController/SwipeTabBarController.m
@@ -0,0 +1,13 @@
+//
+//  SwipeTabBarController.m
+//  SwipeTabBarController
+//
+//  Created by Imanol Viana Sánchez on 14/4/16.
+//  Copyright © 2016 Imanol Viana Sánchez. All rights reserved.
+//
+
+#import "SwipeTabBarController.h"
+
+@implementation SwipeTabBarController
+
+@end
